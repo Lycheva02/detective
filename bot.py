4d22ac0c5e94c7f033a42fd34b8ff6656d5e5b11
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2023-11-21 17:33:14 +0300

    Парсер работает, можно выбрать сценарий и перемещаться между локациями и персонажами

---
+++ bot.py
@@ -0,0 +1,103 @@
+import telebot
+from telebot import types
+import sqlite3
+
+bot = telebot.TeleBot("6017972063:AAHXvzg9U8Rjy821Z8NiQWRZDfuxiXT-Nyo")
+
+# Здесь добавляем состояние для пользователя
+user_states = {}
+cur_loc = ''
+
+def get_locations(scenario_id):
+    conn = sqlite3.connect('game.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM locations WHERE scenario_id = ?", (scenario_id,))
+    locations = cursor.fetchall()
+    conn.close()
+    return [location[0] for location in locations]
+
+def get_characters(location_name, scenario_id):
+    conn = sqlite3.connect('game.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM characters WHERE location_id = (SELECT id FROM locations WHERE name = ? AND scenario_id = ?)", (location_name, scenario_id))
+    characters = cursor.fetchall()
+    conn.close()
+    return [character[0] for character in characters]
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    global user_states
+    keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    btn1 = types.KeyboardButton("Сценарий 1")
+    btn2 = types.KeyboardButton('Сценарий 2')
+    keyboard.add(btn1, btn2)
+    bot.send_message(message.chat.id, "Выберите сценарий:", reply_markup=keyboard)
+    # Начальное состояние пользователя
+    user_states[message.chat.id] = {'state': 'scenario_choice'}
+    bot.register_next_step_handler(message, process_scenario_choice)
+
+def process_scenario_choice(message):
+    global user_states
+    if message.text.startswith('Сценарий'):
+        scenario_id = int(message.text.split()[1])
+        locations = get_locations(scenario_id)
+        reply_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        buttons = [types.KeyboardButton(location) for location in locations]
+        reply_markup.add(*buttons, types.KeyboardButton('Сменить локацию'))
+        bot.send_message(message.chat.id, f"Выберите локацию:", reply_markup=reply_markup)
+        # Обновляем состояние пользователя
+        user_states[message.chat.id] = {'state': 'location_choice', 'scenario_id': scenario_id}
+        bot.register_next_step_handler(message, process_location_choice)
+
+def process_location_choice(message):
+    global cur_loc, user_states
+    scenario_id = user_states[message.chat.id]['scenario_id']
+    if message.text == 'Сменить локацию':
+        # Возвращаем пользователя к выбору локации
+        locations = get_locations(scenario_id)
+        reply_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        buttons = [types.KeyboardButton(location) for location in locations]
+        reply_markup.add(*buttons)
+        bot.send_message(message.chat.id, f"Выберите локацию:", reply_markup=reply_markup)
+        bot.register_next_step_handler(message, process_location_choice)
+        return
+    location_name = message.text
+    cur_loc = location_name
+    bot.send_message(message.chat.id, f"Вы выбрали локацию: {location_name}")
+    characters = get_characters(location_name, scenario_id)
+    reply_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    buttons = [types.KeyboardButton(character) for character in characters]
+    reply_markup.add(*buttons, types.KeyboardButton('Сменить локацию'))
+    bot.send_message(message.chat.id, "Выберите персонажа:", reply_markup=reply_markup)
+    # Обновляем состояние пользователя
+    user_states[message.chat.id]['state'] = 'character_choice'
+    bot.register_next_step_handler(message, process_character_choice)
+
+def process_character_choice(message):
+    global cur_loc, user_states
+    scenario_id = user_states[message.chat.id]['scenario_id']
+    if message.text == 'Сменить персонажа':
+        # Возвращаем пользователя к выбору персонажа
+        location_name = cur_loc
+        characters = get_characters(location_name, scenario_id)
+        reply_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        buttons = [types.KeyboardButton(character) for character in characters]
+        reply_markup.add(*buttons, types.KeyboardButton('Сменить локацию'))
+        bot.send_message(message.chat.id, "Выберите персонажа:", reply_markup=reply_markup)
+        bot.register_next_step_handler(message, process_character_choice)
+        return
+    elif message.text == 'Сменить локацию':
+        locations = get_locations(scenario_id)
+        reply_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        buttons = [types.KeyboardButton(location) for location in locations]
+        reply_markup.add(*buttons)
+        bot.send_message(message.chat.id, f"Выберите локацию:", reply_markup=reply_markup)
+        bot.register_next_step_handler(message, process_location_choice)
+        return
+    character_name = message.text
+    reply_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    reply_markup.add(types.KeyboardButton('Сменить персонажа'), types.KeyboardButton('Сменить локацию'))
+    bot.send_message(message.chat.id, f"Вы выбрали персонажа: {character_name}", reply_markup=reply_markup)
+    bot.register_next_step_handler(message, process_character_choice)
+
+bot.polling()
---
+++ parser.py
@@ -0,0 +1,74 @@
+import sqlite3
+import re
+
+def parse_scenario_file(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+
+    scenario = {}
+    locations = []
+
+    # Получение данных о сценарии
+    scenario_match = re.search(r'\[Сценарий: (.*?)\]', text)
+    if scenario_match:
+        scenario['name'] = scenario_match.group(1)
+
+    desc_match = re.search(r'\[START_DESCRIPTION\](.*?)\[END_DESCRIPTION\]', text, re.DOTALL)
+    if desc_match:
+        scenario['description'] = '\n'.join([line.strip() for line in desc_match.group(1).split('\n')])  # Удаление лишних пробелов
+
+    # Разделение текста по локациям
+    location_texts = re.split(r'- Название локации', text)
+    for loc_text in location_texts[1:]:  # Пропускаем первую часть текста (до первой локации)
+        loc_lines = loc_text.split('\n')
+
+        current_location = {'name': loc_lines[0].strip()}
+        desc_match = re.search(r'\[START\](.*?)\[END\]', loc_text, re.DOTALL)
+        if desc_match:
+            current_location['description'] = '\n'.join([line[2:] for line in desc_match.group(1).split('\n')])  # Удаление лишних пробелов
+
+        photo_match = re.search(r'Фото: (.*?)\n', loc_text)
+        if photo_match:
+            current_location['photo'] = photo_match.group(1)
+
+        char_matches = re.finditer(r'- Имя персонажа (.*?)\[START\](.*?)\[END\]', loc_text, re.DOTALL)
+        characters = []
+        for char_match in char_matches:
+            char_desc = '\n'.join([line[4:] for line in char_match.group(2).split('\n')])  # Удаление лишних пробелов
+            characters.append({'name': char_match.group(1).strip(), 'description': char_desc.strip()})
+
+        current_location['characters'] = characters
+        locations.append(current_location)
+
+    scenario['locations'] = locations
+    return scenario
+
+
+def insert_scenario_to_database(scenario):
+    conn = sqlite3.connect('game.db')  # Подключение к вашей базе данных SQLite
+    cursor = conn.cursor()
+
+    # Вставка данных о сценарии
+    cursor.execute("INSERT INTO scenarios (name, history) VALUES (?, ?)", (scenario['name'], scenario['description']))
+    scenario_id = cursor.lastrowid
+
+    # Вставка данных о локациях
+    for location in scenario['locations']:
+        cursor.execute("INSERT INTO locations (scenario_id, name, description, photo) VALUES (?, ?, ?, ?)",
+                       (scenario_id, location['name'], location['description'], location.get('photo', '')))
+        location_id = cursor.lastrowid
+
+        # Вставка данных о персонажах
+        for character in location.get('characters', []):
+            cursor.execute("INSERT INTO characters (location_id, name, description, photo) VALUES (?, ?, ?, ?)",
+                           (location_id, character['name'], character['description'], character.get('photo', '')))
+
+    conn.commit()
+    conn.close()
+
+# Использование парсера и вставка данных в базу
+file_path = 'test.txt'
+parsed_scenario = parse_scenario_file(file_path)
+insert_scenario_to_database(parsed_scenario)
+
+#6017972063:AAHXvzg9U8Rjy821Z8NiQWRZDfuxiXT-Nyo
