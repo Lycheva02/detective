e0a87c1d858fb7623d0e3e0e210723324796bb12
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2023-11-29 17:44:41 +0300

    Реализован парсинг вопросов

--- parsing/question_parser.py
+++ parsing/question_parser.py
@@ -14,24 +14,14 @@ def parse_questions_file(file_path):
     for i in range(1, len(category_texts), 2):  # Начинаем с 1, так как 0-й элемент - это текст до первой категории
         category_name = category_texts[i]
         category_text = category_texts[i + 1]
-        print(category_name, category_text, sep = '\n')
 
         # Разбиваем текст категории по строкам
         category_lines = category_text.strip().split('\n')
 
-        # Определяем тип контента (вопросы или описание категории)
-        if category_lines[0].strip().isdigit():
-            current_category_id = None
-
-            # Парсим вопросы
-            for line in category_lines:
-                parts = line.strip().split('. ', 1)
-                if len(parts) == 2:
-                    questions.append({'category_name': category_name, 'text': parts[1]})
-
-        elif category_lines[0].strip() == '':
-            # Пропускаем описание категории
-            continue
+        for line in category_lines:
+            parts = line.strip().split('. ', 1)
+            if len(parts) == 2:
+                questions.append({'category_name': category_name, 'text': parts[1]})
 
     return questions
 
@@ -41,7 +31,7 @@ def insert_questions_to_database(questions, scenario_id):
 
     for question in questions:
         # Проверяем существование категории
-        cursor.execute("SELECT id FROM question_categories WHERE scenario_id = ? AND name = ?",
+        cursor.execute("SELECT id FROM question_categories WHERE scenario_id = ? AND category_name = ?",
                        (scenario_id, question['category_name']))
         category_row = cursor.fetchone()
 
@@ -49,13 +39,13 @@ def insert_questions_to_database(questions, scenario_id):
             category_id = category_row[0]
         else:
             # Если категории нет, добавляем ее
-            cursor.execute("INSERT INTO question_categories (scenario_id, name) VALUES (?, ?)",
+            cursor.execute("INSERT INTO question_categories (scenario_id, category_name) VALUES (?, ?)",
                            (scenario_id, question['category_name']))
             category_id = cursor.lastrowid
 
         # Вставляем вопрос
-        cursor.execute("INSERT INTO questions (category_id, text) VALUES (?, ?)",
-                       (category_id, question['text']))
+        cursor.execute("INSERT INTO questions (scenario_id, category_id, question_text) VALUES (?, ?, ?)",
+                       (scenario_id, category_id, question['text']))
 
     conn.commit()
     conn.close()
@@ -63,6 +53,5 @@ def insert_questions_to_database(questions, scenario_id):
 # Использование парсера и вставка данных в базу
 file_path = sys.argv[1]
 parsed_questions = parse_questions_file(file_path)
-print(parsed_questions)
 scenario_id = sys.argv[2]
 insert_questions_to_database(parsed_questions, scenario_id)  # Замените scenario_id на актуальное значение
